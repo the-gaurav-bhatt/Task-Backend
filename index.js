13297292f8dec628108e94abfa9f842bbd1369c6
Gaurav Bhatt <88639820+the-gaurav-bhatt@users.noreply.github.com>
2024-08-11 14:00:31 +0545

    Random Images api updated

--- index.js
+++ index.js
@@ -35,61 +35,61 @@ const insertSampleProducts = async () => {
         {
           name: "Wireless Noise-Canceling Headphones",
           image:
-            "https://images.unsplash.com/photo-1721332149371-fa99da451baa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
+            "https://random-image-pepebigotes.vercel.app/api/random-image",
           price: 150,
         },
         {
           name: "Portable Bluetooth Speaker",
           image:
-            "https://unsplash.com/photos/a-person-using-a-laptop-computer-on-a-table-AoDMssi2UOU",
+            "https://random-image-pepebigotes.vercel.app/api/random-image",
           price: 50,
         },
         {
           name: "Ergonomic Office Chair",
           image:
-            "https://images.unsplash.com/photo-1604426633801-11b2001c5a43?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8b2ZmaWNlJTIwY2hhaXJ8fGVufDB8fDB8fA%3D%3D&w=640&q=80",
+            "https://random-image-pepebigotes.vercel.app/api/random-image",
           price: 200,
         },
         {
           name: "Smartwatch with Fitness Tracking",
           image:
-            "https://images.unsplash.com/photo-1546868873-700ef0109fad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c21hcnR3YXRjaHxlbnwwfHwwfHw%3D&w=640&q=80",
+            "https://random-image-pepebigotes.vercel.app/api/random-image",
           price: 100,
         },
         {
           name: "Espresso Machine with Milk Frother",
           image:
-            "https://images.unsplash.com/photo-1505270933571-2d966c1ef81b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXNwcmVzc28lMjBtYWNoaW5lfGVufDB8fDB8fA%3D%3D&w=640&q=80",
+            "https://random-image-pepebigotes.vercel.app/api/random-image",
           price: 300,
         },
         {
           name: "Wireless Gaming Mouse",
           image:
-            "https://images.unsplash.com/photo-1555449966-26fe89077798?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2FtaW5nJTIwbW91c2V8ZW58MHx8MHx8&w=640&q=80",
+            "https://random-image-pepebigotes.vercel.app/api/random-image",
           price: 80,
         },
         {
           name: "Backpack for Hiking and Travel",
           image:
-            "https://images.unsplash.com/photo-1530745342582-0795f23ec976?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZHVjdHN8ZW58MHx8MHx8fDA%3D",
+            "https://random-image-pepebigotes.vercel.app/api/random-image",
           price: 70,
         },
         {
           name: "Digital Instant Camera",
           image:
-            "https://unsplash.com/photos/white-and-brown-plastic-bottles-nwOip8AOZz0",
+            "https://random-image-pepebigotes.vercel.app/api/random-image",
           price: 120,
         },
 
         {
           name: "Yoga Mat with Carrying Strap",
-          image: "https://unsplash.com/photos/brown-basket-lot-ztgKTnI_eIU",
+          image: "https://random-image-pepebigotes.vercel.app/api/random-image",
           price: 30,
         },
         {
           name: "Cast Iron Skillet",
           image:
-            "https://images.unsplash.com/photo-158384726897-e10576424c86?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FzdCUyMHlyb24lMjBza2lsbGV0fGVufDB8fDB8fA%3D%3D&w=640&q=80",
+            "https://random-image-pepebigotes.vercel.app/api/random-image",
           price: 40,
         },
       ]);
